4b7ede9d953bfa9d4b5c64c52e19c715c3297be1
vee <95954865+veecodes07@users.noreply.github.com>
2022-12-28 22:38:21 +0530

    Add files via upload

---
+++ build/precache-manifest.98337caefdf7387c01ff3e0d40b59ab5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f69285224fe616fc52a",
+    "url": "/static/css/main.722a5868.chunk.css"
+  },
+  {
+    "revision": "5f69285224fe616fc52a",
+    "url": "/static/js/main.5f692852.chunk.js"
+  },
+  {
+    "revision": "f4779e7e36d994c9fdba",
+    "url": "/static/css/1.e2c7d775.chunk.css"
+  },
+  {
+    "revision": "f4779e7e36d994c9fdba",
+    "url": "/static/js/1.f4779e7e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "da70255120845f3066c5bdcc7367f259",
+    "url": "/static/media/tether.da702551.png"
+  },
+  {
+    "revision": "f9f6182ddee9fbdf60c1b3dea274f2fc",
+    "url": "/index.html"
+  }
+];
